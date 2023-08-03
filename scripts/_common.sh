61d233c93087e857d477168d04cea24c25217a1d
woLi <volkanloco@gmail.com>
2023-08-03 19:26:39 +0200

    new
    qq

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,12 +15,12 @@ ynh_delete_file_checksum () {
 # Create a dedicated php-fpm config
 #
 # usage: ynh_add_fpm_config
-ynh_add_fpm7.2_config () {
+ynh_add_fpm8.2_config () {
 	# Configure PHP-FPM 7.1 by default
-	ynh_secure_remove "/etc/php/7.1/fpm/pool.d/$app.conf"   2>&1
-	ynh_secure_remove "/etc/php/7.1/fpm/conf.d/20-$app.ini" 2>&1
-	local fpm_config_dir="/etc/php/7.2/fpm"
-	local fpm_service="php7.2-fpm"
+	ynh_secure_remove "/etc/php/8.1/fpm/pool.d/$app.conf"   2>&1
+	ynh_secure_remove "/etc/php/8.1/fpm/conf.d/20-$app.ini" 2>&1
+	local fpm_config_dir="/etc/php/8.2/fpm"
+	local fpm_service="php8.2-fpm"
 	ynh_app_setting_set $app fpm_config_dir "$fpm_config_dir"
 	ynh_app_setting_set $app fpm_service "$fpm_service"
 	finalphpconf="$fpm_config_dir/pool.d/$app.conf"
@@ -46,7 +46,7 @@ ynh_add_fpm7.2_config () {
 # Remove the dedicated php-fpm config
 #
 # usage: ynh_remove_fpm7.1_config
-ynh_remove_fpm7.2_config () {
+ynh_remove_fpm8.2_config () {
 	local fpm_config_dir=$(ynh_app_setting_get $app fpm_config_dir)
 	local fpm_service=$(ynh_app_setting_get $app fpm_service)
 	ynh_secure_remove "$fpm_config_dir/pool.d/$app.conf"
@@ -81,7 +81,7 @@ exec_composer() {
   shift 1
 
   COMPOSER_HOME="${WORKDIR}/.composer" \
-    sudo /usr/bin/php7.2 "${WORKDIR}/composer.phar" $@ \
+    sudo /usr/bin/php8.2 "${WORKDIR}/composer.phar" $@ \
       -d "${WORKDIR}"  --no-interaction
 }
 
@@ -94,7 +94,7 @@ init_composer() {
   # install composer
   curl -sS https://getcomposer.org/installer \
     | COMPOSER_HOME="${DESTDIR}/.composer" \
-        sudo /usr/bin/php7.2 -- --quiet --install-dir="$DESTDIR" \
+        sudo /usr/bin/php8.2 -- --quiet --install-dir="$DESTDIR" \
     || ynh_die "Unable to install Composer"
 
   # update dependencies to create composer.lock
@@ -106,26 +106,3 @@ sudo_path () {
 	sudo env "PATH=$PATH" $@
 }
 
-#
-# PHP7 helpers
-#
-
-ynh_install_php7 () {
-
-  ynh_package_update
-  ynh_package_install apt-transport-https --no-install-recommends
-
-  wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.list
-
-  ynh_package_update
-  ynh_install_app_dependencies php7.2 php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache php7.2-ldap
-  sudo update-alternatives --install /usr/bin/php php /usr/bin/php5 70
-}
-
-ynh_remove_php7 () {
-  sudo rm -f /etc/apt/sources.list.d/php7.list
-  sudo apt-key del 4096R/89DF5277
-  sudo apt-key del 2048R/11A06851
-  ynh_remove_app_dependencies php7.2 php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache
-}
